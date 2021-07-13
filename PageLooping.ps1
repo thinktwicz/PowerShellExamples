d17abf1c1935a268bcaba06d5f4f335532bd063e
Drew <48705843+thinktwicz@users.noreply.github.com>
2021-07-12 20:18:58 -0400

    Create PageLooping.ps1

---
+++ PageLooping.ps1
@@ -0,0 +1,36 @@
+<#
+  Using GraphAPI and powershell I need to loop through pages to get all infomartion 
+
+#>
+
+# use a bool to know if we are starting on first page
+[bool]$FirstPage = $true
+
+# Looping
+do
+{
+  if($FirstPage)
+  {
+    $Response = Invoke-RestMethod
+  }
+  else
+  {
+    # Head to the next page link
+    $apiURL = $nextLink
+    $Response = Invoke-RestMethod
+  }
+  
+  $nextLink = $Response.'@odata.nextlink'
+  
+  
+  <#
+    do the process of work you want with information from the $Response information as you will jump to next page once done till no more pages
+  #>
+
+}while ($nextLink -ne $null)
+
+
+
+
+
+
